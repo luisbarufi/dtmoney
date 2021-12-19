717c86e2dafa4398f923cdfd6e6be3dc2302d7f9
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2021-12-18 23:42:02 -0300

    summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from "./assets/styles/global";
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
